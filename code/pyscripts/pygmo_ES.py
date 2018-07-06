97c19ec085f226bd1423bdc06473353fee39e103
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-07-06 15:27:50 +0200

    pagmo experiments added. Reimplemented Karpathy/Salimans ES within pagmo

---
+++ code/pyscripts/pygmo_ES.py
@@ -0,0 +1,165 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from IPython import display
+import numpy as np
+plt.rcParams['image.cmap'] = 'gray'
+plt.rcParams['image.interpolation'] = 'nearest'
+get_ipython().run_line_magic('matplotlib', 'inline')
+import pygmo as pg
+from pygmo import algorithm
+import os
+import sys
+import json
+
+import time
+import numpy as np
+from random import shuffle
+
+
+# In[2]:
+
+
+import inspect
+import math
+
+
+# In[3]:
+
+
+# generate a toy 2D regression dataset
+sz = 100
+X,Y = np.meshgrid(np.linspace(-1,1,sz),np.linspace(-1,1,sz))
+mux,muy,sigma=0.3,-0.3,4
+G1 = np.exp(-((X-mux)**2+(Y-muy)**2)/2.0*sigma**2)
+mux,muy,sigma=-0.3,0.3,2
+G2 = np.exp(-((X-mux)**2+(Y-muy)**2)/2.0*sigma**2)
+mux,muy,sigma=0.6,0.6,2
+G3 = np.exp(-((X-mux)**2+(Y-muy)**2)/2.0*sigma**2)
+mux,muy,sigma=-0.4,-0.2,3
+G4 = np.exp(-((X-mux)**2+(Y-muy)**2)/2.0*sigma**2)
+G = G1 + G2 - G3 - G4
+fig,ax = plt.subplots()
+im = ax.imshow(G, vmin=-1, vmax=1, cmap='jet')
+#plt.axis('off')
+
+
+# In[4]:
+
+
+class saddle_space:
+    def __init__(self):
+        self.dim = 2
+    
+    def fitness(self,x):
+        return [G[int(x[0]),int(x[1])]]
+    
+    def get_bounds(self):
+        return ([0,0],[99,99])
+    
+    def get_name(self):
+        return f"saddlespace"
+    
+    def plot(self, w, idx):
+        plt.imshow(G,vmin=-1, vmax=1, cmap='jet')
+        x,y = zip(*w)
+        plt.scatter(x,y,4,'k',edgecolors='face')
+        plt.scatter(w[idx][0],w[idx][1],15,'y',edgecolors='k')
+
+
+# In[5]:
+
+
+class salimans_nes:
+    def __init__(self,iter=12):
+        super(salimans_nes,self).__init__()
+        self.prevx,self.prevy = [],[]
+        
+        self.iter=iter
+    
+    def evolve(self,pop):
+        if len(pop) == 0:
+            return pop
+        sigma = 3
+        alpha = 0.03 # learningrate
+        
+        # plotting
+        plotting = False
+        if plotting:
+            plt.figure(figsize=(self.iter,self.iter))
+            no_rows = int(self.iter/4+1)
+            gs = gridspec.GridSpec(no_rows,4)
+            plot_index = 0
+        
+        #for each iteration, jitter around starting points, and move in the
+        #best direction (weighted average jitter coordinates according to 
+        #fitness score)
+        for i in range(self.iter):
+            
+            if plotting:
+                ax1=plt.subplot(gs[int(i/4),plot_index])
+                plot_index += 1
+                if plot_index == 4:
+                    plot_index = 0
+                plt.imshow(G,vmin=-1, vmax=1, cmap='jet')
+            
+            #get the population    
+            wl = pop.get_x()
+            
+            #do the jittering and selection
+            j=0
+            for w in wl:
+                noise = np.random.randn(200,2)
+                wp = [[min(99,max(0,x)),min(99,max(0,y))] for [x,y] in np.expand_dims(w, 0) + sigma*noise]
+                
+                
+                
+                if plotting:
+                    x,y = zip(*wp)
+                    plt.scatter(x,y,4,'k',edgecolors='face')
+                R = np.array([prob.fitness(wi)[0] for wi in wp])
+                R -= R.mean()
+                R /= R.std()
+                g = np.dot(R, noise)
+                u = alpha * g
+                w += u # mutate the population
+                w = [min(99,max(0,w[0])),min(99,max(0,w[1]))] # bounds
+                pop.set_x(j,w)# make the move previously selected
+                j+=1
+        return pop
+
+    def get_name(self):
+        return f"Oisin's big-dick omegafantastic algorithm"
+
+
+# In[8]:
+
+
+def pygmo_es():
+    uda = salimans_nes(iter=25)
+    udp=saddle_space()
+    prob=pg.problem(udp)
+
+    archi = pg.archipelago(algo=uda, prob=udp, n=10, pop_size=30)
+    archi.evolve()
+    sols = archi.get_champions_f()
+    idx = sols.index(min(sols))
+    print("Done!! Solutions found are: ")
+    print(archi.get_champions_f())
+    udp.plot(archi.get_champions_x(),idx)
+
+    #pop = pg.population(prob,10,3)
+    #algo.evolve(pop)
+
+
+# In[ ]:
+
+
+if __name__ == '__main__':
+    pygmo_es()
+
