21585df9f8f2b072b717d101851d741dfa192c87
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-08-08 10:43:43 +0200

    unimportant ipynb and run_sim changes.

--- code/run_sim.py
+++ code/run_sim.py
@@ -15,14 +15,14 @@ if __name__ == "__main__":
     # print(round(time.time() - starttime, 3))
 
     starttime = time.time()
-    # # hohmann
-    # psi = [
-    #     -2.086814820119193,
-    #     -0.000122173047640,
-    #     3.111181716545691 / unit_velocity,
-    # ]
-    # path = launch_sim(psi)
-    # orbitplot2d(path, psi, title="hohmann")
+    # hohmann
+    psi = [
+        -2.086814820119193,
+        -0.000122173047640,
+        3.111181716545691 / unit_velocity,
+    ]
+    path = launch_sim(psi)
+    orbitplot2d(path, psi, title="hohmann")
 
     # reverse hohmann
     psi = [
@@ -34,32 +34,32 @@ if __name__ == "__main__":
     orbitplot2d(path, psi, title="reverse hohmann")
     orbitplot_non_inertial(path, psi, title="reverse_hohmann")
 
-    # # low energy long
-    # psi = [3.794182930145708, 0.023901745288554, 3.090702702702703 / unit_velocity]
-    # path = launch_sim(psi)
-    # orbitplot2d(path, psi, title="LE long")
+    # low energy long
+    psi = [3.794182930145708, 0.023901745288554, 3.090702702702703 / unit_velocity]
+    path = launch_sim(psi)
+    orbitplot2d(path, psi, title="LE long")
 
-    # # low energy short
-    # psi = [
-    #     -0.138042744751570,
-    #     -0.144259374836607,
-    #     3.127288444444444 / unit_velocity,
-    # ]
-    # path = launch_sim(psi)
-    # orbitplot2d(path, psi, title="LE short")
+    # low energy short
+    psi = [
+        -0.138042744751570,
+        -0.144259374836607,
+        3.127288444444444 / unit_velocity,
+    ]
+    path = launch_sim(psi)
+    orbitplot2d(path, psi, title="LE short")
 
-    # # 3-day-hohmann
-    # psi = [
-    #     -2.272183066647597,
-    #     -0.075821466029764,
-    #     3.135519748743719 / unit_velocity,
-    # ]
-    # path = launch_sim(psi)
-    # orbitplot2d(path, psi, title="3-day-hohmann")
+    # 3-day-hohmann
+    psi = [
+        -2.272183066647597,
+        -0.075821466029764,
+        3.135519748743719 / unit_velocity,
+    ]
+    path = launch_sim(psi)
+    orbitplot2d(path, psi, title="3-day-hohmann")
 
-    # # 1-day-hohmann
-    # psi = [-2.277654673852600, 0.047996554429844, 3.810000000000000 / unit_velocity]
-    # path = launch_sim(psi)
-    # orbitplot2d(path, psi, title="1-day-hohmann")
+    # 1-day-hohmann
+    psi = [-2.277654673852600, 0.047996554429844, 3.810000000000000 / unit_velocity]
+    path = launch_sim(psi)
+    orbitplot2d(path, psi, title="1-day-hohmann")
 
     print(round(time.time() - starttime, 3))
