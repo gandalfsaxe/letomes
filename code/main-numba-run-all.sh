7a0dfcc375a18b1dd2658908cd010ae755a9082e
Gandalf Saxe <gandalfsaxe@gmail.com>
2018-08-09 13:36:05 +0200

    Bash script: run all demos/searches of main_numba.py

---
+++ code/main-numba-run-all.sh
@@ -0,0 +1,14 @@
+### Runs all demos and seaches ###
+
+# Precalculated initial conditions for specific orbit types
+python main_numba.py leo
+python main_numba.py llo
+python main_numba.py h
+python main_numba.py h3
+python main_numba.py h1
+python main_numba.py hr
+python main_numba.py ls
+python main_numba.py ll
+# Search for trajectories
+python main_numba.py sh
+python main_numba.py sl
