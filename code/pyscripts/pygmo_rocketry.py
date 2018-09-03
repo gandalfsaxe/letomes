04cd28480df0a3cc33b98f0413f87c093f9995e3
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-09-03 13:33:52 +0200

    pygmo_rocketry HPC export

--- code/pyscripts/pygmo_rocketry.py
+++ code/pyscripts/pygmo_rocketry.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
 import matplotlib.pyplot as plt
@@ -29,14 +29,13 @@ pi4=pi/4
 pi2=pi/2
 
 
-# In[2]:
+# In[6]:
 
 
 class saddle_space:
     def __init__(self):
         self.dim = 3
     
-    @jit
     def fitness(self,psi):
         res,_ = launch_sim(psi, duration= 50/UNIT_TIME, max_iter=1e7)
         return [-res]
@@ -52,7 +51,7 @@ class saddle_space:
         pass
 
 
-# In[3]:
+# In[7]:
 
 
 class salimans_nes:
@@ -61,7 +60,6 @@ class salimans_nes:
 
         self.iter = iter  # number of steps towards estimated gradient
     
-    @jit
     def evolve(self, pop):
         if len(pop) == 0:
             return pop
@@ -90,44 +88,46 @@ class salimans_nes:
                 g = np.dot(R, noise)
 #                 print(f"R = {R}, g = {g}")
                 u = alpha * g
-#                 print(f"new individual = {str(u)}")
-                w += u  # mutate the population
+                print(f"new individual = {str(u)}")
+                w += u  # mutate the population/take the step
 
                 pop.set_x(j, w)  # make the move previously selected
                 j += 1
         return pop
 
     def get_name(self):
-        return f"Oisin's big-dick omegafantastic algorithm"
+        return f"Salimans_NES"
 
 
-# In[4]:
+# In[13]:
 
 
 def pygmo_es():
-    uda = salimans_nes(iter=100)  # user defined algorithm
+    ARCHIPELAGO=True
+    iterations = 10
+        
+    uda = salimans_nes(iter=iterations)  # user defined algorithm
     udp = saddle_space()  # user defined problem
     prob = pg.problem(udp) # Beautiful white snow
+    
 
-    archi = pg.archipelago(algo=uda, prob=udp, n=8, pop_size=20)
-    archi.evolve()
-    print(archi)
-    archi.wait()
+    if ARCHIPELAGO:
+        archi = pg.archipelago(algo=uda, prob=prob, n=1, pop_size=4)
+        archi.evolve()
+#         print(archi) # prints description of islands contained in archipelago
+#         archi.wait()
+        sols = archi.get_champions_f()
+        idx = sols.index(min(sols))
+        sols_x = archi.get_champions_x()
+        sol=sols[idx],sols_x[idx]
+    else:
+        pop = pg.population(prob=prob, size=4)
+        pop.set_x(0,[-2.277654673852600, 0.047996554429844, 3.810000000000000])
+        pop.set_x(1,[-0.138042744751570, -0.144259374836607, 3.127288444444444])
+        pop.set_x(2,[-2.086814820119193, -0.000122173047640, 3.111181716545691])
+        uda.evolve(pop)
+        sol = (pop.champion_f, pop.champion_x)
 
-    #     pop = pg.population(prob=udp, size=4)
-    #     pop.set_x(0,[-2.277654673852600, 0.047996554429844, 3.810000000000000])
-    #     pop.set_x(1,[-0.138042744751570, -0.144259374836607, 3.127288444444444])
-    #     pop.set_x(2,[-2.086814820119193, -0.000122173047640, 3.111181716545691])
-    #     uda.evolve(pop)
-    #     udi=pg.island(algo=uda, pop=pop)
-    #     udi.evolve()
-    #     sol = udi.get_population()
-    sols = archi.get_champions_f()
-    idx = sols.index(min(sols))
-    sols_x = archi.get_champions_x()
-    sol=sols[idx],sols_x[idx]
-    #     sol = (pop.champion_f, pop.champion_x)
-    print(sols,sols_x)
     print("Done! best solution is:")
     print(sol)
     return sol
