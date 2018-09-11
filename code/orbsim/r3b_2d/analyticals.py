40aa6dcafe1208419e976945d9a8882bdf2f6fff
Gandalf Saxe <gandalfsaxe@gmail.com>
2018-09-11 09:51:03 +0200

    docstrings + removed k from input argument

--- code/orbsim/r3b_2d/analyticals.py
+++ code/orbsim/r3b_2d/analyticals.py
@@ -12,8 +12,8 @@ from . import k
 
 @njit
 def get_pdot_x(x, y, p_y):
-    """from position and momentum vectors, returns generalized momentum, nondimensionalized"""
-    denominator_1, denominator_2 = pdot_denominators(x, y, k)
+    """generalized momentum p_x (nondimensionalized) from position and momentum vectors"""
+    denominator_1, denominator_2 = pdot_denominators(x, y)
     pdot_x = (
         p_y - ((1 - k) * (x + k)) / denominator_1 - k * (x - 1 + k) / denominator_2
     )  # Note: In old version there was a sign error and "1+k-x" used to be "1-k-x"
@@ -22,13 +22,15 @@ def get_pdot_x(x, y, p_y):
 
 @njit
 def get_pdot_y(x, y, p_x):
-    denominator_1, denominator_2 = pdot_denominators(x, y, k)
+    """generalized momentum p_y (nondimensionalized) from position and momentum vectors"""
+    denominator_1, denominator_2 = pdot_denominators(x, y)
     pdot_y = -p_x - 1 + k * y / denominator_1 - k * y / denominator_2
     return pdot_y
 
 
 @njit
-def pdot_denominators(x, y, k):
+def pdot_denominators(x, y):
+    """denominators used in get_pdot_x and get_pdot_y"""
     denominator_1 = ((x + k) ** 2 + y ** 2) * sqrt((x + k) ** 2 + y ** 2)
     denominator_2 = ((x - 1 + k) ** 2 + y ** 2) * sqrt(
         (x - 1 + k) ** 2 + y ** 2
@@ -38,11 +40,13 @@ def pdot_denominators(x, y, k):
 
 @njit
 def get_v_x(y, p_x):
+    """speed in x direction, from coordinates and momenta"""
     v_x = p_x + y
     return v_x
 
 
 @njit
 def get_v_y(x, p_y):
+    """speed in y direction, from coordinates and momenta"""
     v_y = p_y - x
     return v_y
