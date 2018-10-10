45ab7e87471cabc80ef709b5aa106a68eb59ad33
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-10-10 13:01:22 +0200

    added disjoint intervals randomizer to analyticals

--- code/orbsim/r3b_2d/analyticals.py
+++ code/orbsim/r3b_2d/analyticals.py
@@ -4,6 +4,8 @@ Derived via Hamiltons's equations.
 """
 
 from math import sqrt
+import numpy as np
+import numpy.random as npr
 
 from numba import njit
 
@@ -50,3 +52,36 @@ def get_ydot(x, p_y):
     """speed in y direction, from coordinates and momenta"""
     v_y = p_y - x
     return v_y
+
+
+@njit
+def collapse_intervals(bounds):
+    lens = []
+    for i in range(len(bounds)):
+        lb = bounds[i][0]
+        ub = bounds[i][1]
+        lens.append(ub - lb)
+    lens = np.array(lens)
+    return lens.cumsum()
+
+
+@njit
+def random_disjoint_intervals(bounds):
+    """
+    input: bounds = np.array([[lowerbound0,upperbound0],[lb1,ub1],..,[lbn,ubn]]), must be disjoint, and sorted.
+    returns: single uniform random number within the union of the given intervals.
+    """
+    lens = collapse_intervals(bounds)
+    total_len = lens[-1]
+
+    R = npr.rand() * total_len
+
+    idx = 0
+    for i in range(len(bounds)):
+        if R <= lens[i]:
+            idx = i
+            break
+    lb = bounds[idx][0]
+    ub = bounds[idx][1]
+    R_star = (ub - lb) * npr.rand() + lb
+    return R_star
