3f67edb7f6b4f2989c3ce90e914e663f916d1fde
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-12-08 19:03:35 +0100

    comparator updates

--- code/pyscripts/comparative.py
+++ code/pyscripts/comparative.py
@@ -2,6 +2,7 @@
 from matplotlib.colors import Normalize
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import numpy as np
 from math import pi
@@ -24,22 +25,37 @@ def evolve(startpsi, iterations, timeline):
     alpha = 0.5
     epsi_size = 25
     psi = startpsi
+    best_score = 100.0
     for i in range(int(iterations / epsi_size)):
         noise = np.random.randn(epsi_size, 2).astype("int")
         x, y = psi
         x, y = [min(dims[0] - 1, max(0, x)), min(dims[1] - 1, max(0, y))]
         epsi = [x, y] + sigma * noise
-        timeline.append({"score": pspace[int(x)][int(y)], "coords": [x, y]})
 
-        R = np.array([-1 * pspace[int(x)][int(y)] for x, y in epsi])
+        score = pspace[int(x)][int(y)]
+        if score < best_score:
+            best_score = score
+
+        R = np.array(
+            [
+                -1
+                * pspace[int(min(dims[0] - 1, max(0, x)))][
+                    int(min(dims[1] - 1, max(0, y)))
+                ]
+                for x, y in epsi
+            ]
+        )
         R -= R.mean()
         R /= R.std()
         step_norm = np.dot(R, noise)
         step = alpha * step_norm
         psi += step
+        timeline.append(
+            {"score": score, "best_score": best_score, "coords": [x, y], "step": step}
+        )
 
 
-eval_budget = 10000
+eval_budget = 1000
 best_run = {"score": 100.0, "coords": [0, 0]}
 rg_timeline = []
 x_timeline = []
@@ -52,7 +68,9 @@ for _ in range(eval_budget):
         best_run["score"] = score
         best_run["coords"] = psi
 
-    rg_timeline.append(best_run.copy())
+    rg_timeline.append(
+        {"best_score": best_run["score"], "score": score, "coords": best_run["coords"]}
+    )
     x_timeline.append(psi[0])
     y_timeline.append(psi[1])
 
@@ -66,8 +84,17 @@ axrand = fig.add_subplot("221")
 axevos = fig.add_subplot("222")
 ax_rg_pspace = fig.add_subplot("223")
 ax_es_pspace = fig.add_subplot("224")
-axrand.plot([best["score"] for best in rg_timeline], color="black")
-axevos.plot([part["score"] for part in es_timeline], color="red")
+cummean = np.array(
+    pd.Series([part["score"] for part in es_timeline]).expanding().mean()
+)
+print(f"cummean = {cummean}")
+axrand.plot([part["best_score"] for part in rg_timeline], color="black")
+axrand.plot([part["score"] for part in rg_timeline], color="red", alpha=0.4)
+
+axevos.plot([part["score"] for part in es_timeline], color="red", alpha=0.4)
+axevos.plot(cummean, "b-")
+axevos.plot([part["best_score"] for part in es_timeline], color="black")
+
 
 cmap = plt.cm.jet
 colors = Normalize(min(pspace.flatten()), max(pspace.flatten()))(pspace)
@@ -89,11 +116,24 @@ im2 = ax_es_pspace.imshow(
 
 plt.colorbar(mappable=im, ax=ax_rg_pspace)
 bestcoords = np.array([best["coords"] for best in rg_timeline]).T
-ax_rg_pspace.scatter(bestcoords[0], bestcoords[1], color="lime", s=3)
 ax_rg_pspace.scatter(x_timeline, y_timeline, color="black", s=0.2)
+ax_rg_pspace.scatter(bestcoords[0], bestcoords[1], color="lime", s=3)
 
 plt.colorbar(mappable=im2, ax=ax_es_pspace)
-coords = np.array([part["coords"] for part in es_timeline]).T
-ax_es_pspace.scatter(coords[0], coords[1], color="black", s=3)
+coords = np.array([part["coords"] for part in es_timeline])
+steps = np.array([part["step"] for part in es_timeline])
+ax_es_pspace.plot(coords.T[0], coords.T[1], "wo-")
+ax_es_pspace.scatter(coords.T[0], coords.T[1], color="black", s=6)
+arrow = [coords[-1], steps[-1]]
+ax_es_pspace.arrow(
+    arrow[0][0],
+    arrow[0][1],
+    arrow[1][0],
+    arrow[1][1],
+    head_width=1,
+    head_length=1.5,
+    fc="lime",
+    ec="lime",
+)
 plt.show()
 
