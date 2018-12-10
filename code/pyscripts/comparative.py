e991f2748c2b8e00211d3fe4ad9634c0efcc61c0
Oisin Daly Kiaer <oisin.daly.kiaer@gmail.com>
2018-12-10 11:42:39 +0100

    comparator updates

--- code/pyscripts/comparative.py
+++ code/pyscripts/comparative.py
@@ -5,57 +5,76 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import numpy as np
-from math import pi
+from math import pi, log
+from scipy.stats import rankdata
 
-pspace = np.loadtxt("golf_course_zoom.txt")
-print(pspace)
+# === setup problem space, either real or Karpathy toy problem for validation ===
+np.random.seed(8)
+pspace = np.loadtxt("golf_course_zoom_s1024.txt")
+sz = 1024
+X, Y = np.meshgrid(np.linspace(-1, 1, sz), np.linspace(-1, 1, sz))
+mux, muy, sigma = 0.3, -0.3, 4
+G1 = np.exp(-((X - mux) ** 2 + (Y - muy) ** 2) / 2.0 * sigma ** 2)
+mux, muy, sigma = -0.3, 0.3, 2
+G2 = np.exp(-((X - mux) ** 2 + (Y - muy) ** 2) / 2.0 * sigma ** 2)
+mux, muy, sigma = 0.6, 0.6, 2
+G3 = np.exp(-((X - mux) ** 2 + (Y - muy) ** 2) / 2.0 * sigma ** 2)
+mux, muy, sigma = -0.4, -0.2, 3
+G4 = np.exp(-((X - mux) ** 2 + (Y - muy) ** 2) / 2.0 * sigma ** 2)
+G = G1 + G2 - G3 - G4
+
+# uncomment this line if you want smooth toy-problem
+# pspace = G
 dims = pspace.shape
 print(dims)
 
-tau = pi * 2
-bounds = {
-    "pos": np.array([[3.8, 5.0]]),
-    "ang": np.array([[0, 0.02]]),
-    "burn": np.array([[3.1, 3.15]]),
-}
+startpsi = [200, 400]
+# startpsi = [np.random.randint(low=0, high=dim) for dim in dims]
+sigma = 25
+alpha = 300
+epsi_size = 200
+eval_budget = 10000
 
-
+# +++++++++++++++++++++ Evolution Strategies ++++++++++++++++++++++++++++++
 def evolve(startpsi, iterations, timeline):
-    sigma = 5
-    alpha = 0.5
-    epsi_size = 25
     psi = startpsi
     best_score = 100.0
     for i in range(int(iterations / epsi_size)):
-        noise = np.random.randn(epsi_size, 2).astype("int")
+        noise = np.random.randn(epsi_size, 2)
         x, y = psi
         x, y = [min(dims[0] - 1, max(0, x)), min(dims[1] - 1, max(0, y))]
         epsi = [x, y] + sigma * noise
+        epsi = [
+            [min(dims[0] - 1, max(0, x)), min(dims[1] - 1, max(0, y))] for x, y in epsi
+        ]
 
         score = pspace[int(x)][int(y)]
         if score < best_score:
             best_score = score
 
-        R = np.array(
-            [
-                -1
-                * pspace[int(min(dims[0] - 1, max(0, x)))][
-                    int(min(dims[1] - 1, max(0, y)))
-                ]
-                for x, y in epsi
-            ]
-        )
-        R -= R.mean()
-        R /= R.std()
+        R = np.array([-1 * pspace[int(x), int(y)] for x, y in epsi])
+        R = rankdata(R)
+        Rmean = R.mean()
+        R = np.array([max(Rmean, v) for v in R])
+        R /= sum(R)
         step_norm = np.dot(R, noise)
         step = alpha * step_norm
         psi += step
         timeline.append(
-            {"score": score, "best_score": best_score, "coords": [x, y], "step": step}
+            {
+                "score": score,
+                "best_score": best_score,
+                "coords": [x, y],
+                "step": step,
+                "epsi": epsi,
+            }
         )
 
 
-eval_budget = 1000
+es_timeline = []
+evolve(startpsi, eval_budget, es_timeline)
+
+# ===================== RANDOM GUESSING ==================================
 best_run = {"score": 100.0, "coords": [0, 0]}
 rg_timeline = []
 x_timeline = []
@@ -74,28 +93,30 @@ for _ in range(eval_budget):
     x_timeline.append(psi[0])
     y_timeline.append(psi[1])
 
-es_timeline = []
-startpsi = [np.random.randint(low=0, high=dim) for dim in dims]
-evolve(startpsi, eval_budget, es_timeline)
-
-
+# ******************** PLOTTING ****************************************
+# ======== establish figs =================
 fig = plt.figure()
 axrand = fig.add_subplot("221")
 axevos = fig.add_subplot("222")
 ax_rg_pspace = fig.add_subplot("223")
 ax_es_pspace = fig.add_subplot("224")
+
+# ========= compute and plot convergence curves ==============
+cummean = np.array(
+    pd.Series([part["score"] for part in rg_timeline]).expanding().mean()
+)
+axrand.plot([part["best_score"] for part in rg_timeline], color="black")
+axrand.plot(cummean, "b-")
+axrand.plot([part["score"] for part in rg_timeline], color="red", alpha=0.4)
+
 cummean = np.array(
     pd.Series([part["score"] for part in es_timeline]).expanding().mean()
 )
-print(f"cummean = {cummean}")
-axrand.plot([part["best_score"] for part in rg_timeline], color="black")
-axrand.plot([part["score"] for part in rg_timeline], color="red", alpha=0.4)
-
 axevos.plot([part["score"] for part in es_timeline], color="red", alpha=0.4)
 axevos.plot(cummean, "b-")
 axevos.plot([part["best_score"] for part in es_timeline], color="black")
 
-
+# ============= plot problem space bg images ====
 cmap = plt.cm.jet
 colors = Normalize(min(pspace.flatten()), max(pspace.flatten()))(pspace)
 colors = cmap(colors)
@@ -114,14 +135,22 @@ im2 = ax_es_pspace.imshow(
     interpolation="none",
 )
 
+# ========= colorbars =========================
 plt.colorbar(mappable=im, ax=ax_rg_pspace)
 bestcoords = np.array([best["coords"] for best in rg_timeline]).T
 ax_rg_pspace.scatter(x_timeline, y_timeline, color="black", s=0.2)
 ax_rg_pspace.scatter(bestcoords[0], bestcoords[1], color="lime", s=3)
 
 plt.colorbar(mappable=im2, ax=ax_es_pspace)
+epsis = np.array([part["epsi"] for part in es_timeline])
 coords = np.array([part["coords"] for part in es_timeline])
 steps = np.array([part["step"] for part in es_timeline])
+
+# ES timeline plot
+# fig2 = plt.figure()
+for idx, epsi in enumerate(epsis):
+    ax_es_pspace.scatter(np.array(epsi).T[0], np.array(epsi).T[1], color="black", s=0.2)
+
 ax_es_pspace.plot(coords.T[0], coords.T[1], "wo-")
 ax_es_pspace.scatter(coords.T[0], coords.T[1], color="black", s=6)
 arrow = [coords[-1], steps[-1]]
@@ -130,10 +159,12 @@ ax_es_pspace.arrow(
     arrow[0][1],
     arrow[1][0],
     arrow[1][1],
-    head_width=1,
-    head_length=1.5,
+    head_width=3,
+    head_length=3,
     fc="lime",
     ec="lime",
 )
+
+
 plt.show()
 
